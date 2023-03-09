90df5efaca2a45f757a6a110adac68bc70114b71
Maria Nuila <quantum_healer@Marias-MBP.lan>
2023-03-08 19:45:03 -0800

    routes.py was updated
    Completed the index function

--- climate_app/routes.py
+++ climate_app/routes.py
@@ -1,8 +1,20 @@
-from myapp import app
+from climate_app import app
 import json, plotly
 
 from flask import render_template
-#from wrangling_scripts.wrangling_data import return_figures
+from wrangling_scripts.wrangling_data import return_figures
 
 @app.route('/')
 @app.route('/index')
+def index():
+    
+    figures = return_figures()
+
+    #plot ids for html id tag
+    ids = ['figure-{}'.format(i) for i, _ in enumerate(figures)]
+    #Convert the plotly figures to JSON for javascript in HTML template
+    figuresJSON = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template('index.html',
+                           ids=ids,
+                           figuresJSON=figuresJSON)
+
