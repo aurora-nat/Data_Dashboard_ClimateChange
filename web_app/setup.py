36ecb22c532bdc1a0589f1ed3391f89f9b6f4510
Maria Nuila <quantum_healer@Marias-MBP.lan>
2023-03-14 15:42:44 -0700

    Adding a setup.py file to webapp directory

---
+++ web_app/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A Data Dashboard with Climate Change World Indicators'
+
+setup(name='Data Dashboard',
+      version=VERSION,
+      author='Nataly Nuila',
+      author_email='<nataly.nuila@gmail.com>',
+      description=DESCRIPTION,
+      packages=['data_dashboard'],
+      zip_safe=False)
